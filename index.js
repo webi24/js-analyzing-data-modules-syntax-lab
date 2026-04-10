9dae5578bf94e7e736d103eaf58a2a59638b4fc1
Code Gaby <webigabriel@gmail.com>
2026-04-10 18:29:31 +0300

    Completed combineusers function

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require('datejs')
 
+function combineUsers(...args) {
+  let combinedObject = {
+    users: []
+  }
 
+  for (let array of args) {
+    combinedObject.users = [...combinedObject.users, ...array]
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy')
+
+  return combinedObject
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
